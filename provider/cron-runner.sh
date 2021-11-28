7fed4342501c33a788208f9687a3c79d7d265849
Tim Porritt <tim.m.porritt@gmail.com>
2021-11-29 01:33:38 +1100

    Copy contents not folder

--- provider/cron-runner.sh
+++ provider/cron-runner.sh
@@ -5,5 +5,5 @@ set -e
 . ./venv/bin/activate
 python3 ./process.py --single $1
 
-cp -a ../conf-out /etc/nginx/sites-available
+cp -a ../conf-out/* /etc/nginx/sites-available
 systemctl reload nginx
