e7a4f24d500cd8308d23db45eb22731c0d7421cb
Tim Porritt <tim.m.porritt@gmail.com>
2021-11-29 00:39:45 +1100

    Specify bash in shebang

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
-SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Provider
 apt-get install -y python3-venv
