3633ccfbab03fe96e5a1ae9ca34d4d082c2c2bec
Tim Porritt <tim.m.porritt@gmail.com>
2021-11-30 23:35:52 +1100

    Fix typo

--- provider/cron-runner.sh
+++ provider/cron-runner.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 
 . ./venv/bin/activate
-python3 /process.py --single $1
+python3 ./process.py --single $1
 
 cp -a ../conf-out/* /etc/nginx/sites-available
 systemctl reload nginx
