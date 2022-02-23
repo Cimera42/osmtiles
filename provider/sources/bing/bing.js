0ab573f79cc15081f414d9e2524ba98d296bed0c
Tim Porritt <tim.m.porritt@gmail.com>
2022-02-24 00:59:11 +1100

    Use nginx JS instead of lua

---
+++ provider/sources/bing/bing.js
@@ -0,0 +1,44 @@
+function xyzToQuadkey(x, y, z) {
+  let quadkey = "";
+
+  for (let i = z; i > 0; i--) {
+    let digit = 0;
+    const mask = 1 << (i - 1);
+
+    if (x & (mask != 0)) {
+      digit = digit + 1;
+    }
+    if (y & (mask != 0)) {
+      digit = digit + 2;
+    }
+
+    quadkey += digit.toString();
+  }
+
+  return quadkey;
+}
+
+const subdomainMap = {
+  a: "0",
+  b: "1",
+  c: "2",
+};
+
+function getUrl(req) {
+  const uri = req.uri;
+  const match =
+    /^\/source\/bing\/(?<sw>[abc])\/(?<z>\d+)\/(?<x>\d+)\/(?<y>\d+)$/.match(
+      uri
+    );
+  if (match) {
+    const subdomain = subdomainMap[match.groups["sw"]];
+    const x = match.groups["x"];
+    const y = match.groups["y"];
+    const z = match.groups["z"];
+    return `https://ecn.t${subdomain}.tiles.virtualearth.net/tiles/a${xyzToQuadkey(
+      x,
+      y,
+      z
+    )}.jpeg?g=587&n=z`;
+  }
+}
